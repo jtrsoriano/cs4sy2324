6c6b4091dcfb127220be9e59c7f8bae64e90d733
CHARM <CHARM@172.22.16.86>
2023-08-29 16:14:10 +0800

    First java project

---
+++ src/cs4charmsoriano/CS4CharmSoriano.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4charmsoriano;
+
+/**
+ *
+ * @author CHARM
+ */
+public class CS4CharmSoriano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int one = 1;
+        System.out.println("hello");
+        int two = 2;
+        System.out.println("var two is greater than three: " + (one > two)  );
+    }
+    
+}
